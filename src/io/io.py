05f4d899490fc654e6f27a04be8a3826a7301ed6
LAUHank <liuhongliang@myhope365.com>
2018-10-26 08:46:29 +0800

    io update

--- src/io/io.py
+++ src/io/io.py
@@ -5,6 +5,23 @@ import os
 import random
 import sys
 
+def readFileWhile(path='io.txt', mode='r') :
+    f = open(path, mode)
+    flag = True
+    while flag :
+        c = f.read(1) # f.readline() # f.read(1)
+        if c : sys.stdout.write(c)
+        else : flag = False
+    f.close()
+
+def readFileWhile1(path='io.txt', mode='r') :
+    f = open(path, mode)
+    c = f.readline() # f.read(1)
+    while c :
+        sys.stdout.write(c)
+        c = f.readline() # f.read(1)
+    f.close()
+
 def readFile(filePath = "a.txt", fileMode = "r") :
     try :
         file = open(filePath, fileMode)
@@ -14,7 +31,7 @@ def readFile(filePath = "a.txt", fileMode = "r") :
     except Exception, e :
         print e
     finally :
-        if file : file.close()
+        file.close()
     print "%s_%s_done" % (filePath, fileMode)
 
 def writeFile(filePath = "a.txt", fileMode = "a", ctnt = '写文件测试', eol = os.linesep) :
@@ -32,8 +49,9 @@ def writeFile(filePath = "a.txt", fileMode = "a", ctnt = '写文件测试', eol 
 def test() :
     filePath = 'my_io_file.txt' # '/root/Downloads/io'
     # 无论是读还是写, Python会自动将\n转换成相应OS的换行符
-    writeFile(filePath, fileMode='a', eol='\n') 
+    # writeFile(filePath, fileMode='a', eol='\n') 
     # readFile(filePath)
+    readFileWhile(path = filePath)
 
 if __name__ == '__main__':
     test()
